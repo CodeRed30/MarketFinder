6c0f27d2ccde7bad9722007a6c7548f24e411e32
CodeRed30 <katrinaharradine@gmail.com>
2021-03-17 16:42:03 +0000

    API responses (with bad localhost)

--- App.js
+++ App.js
@@ -1,8 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+const axios = require('axios').default;
+
+const serverMarketsURL = (process.env.REACT_APP_SERVER || "http://localhost:3000/") + "markets"
+
+let output = fetch('http://192.168.1.110:3000')
+
+// axios.get('http://192.168.1.110:3000/markets')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response.data[2].name);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
+
+  axios({
+    url: 'http://192.168.1.110:3000/markets',
+    method: 'get'
+  }).then(response => {
+    console.log(response.data);
+  }).catch(function (error) {
+    // handle error
+    console.log(error);
+  });
 
 export default function App() {
+console.log('this' + output)
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
