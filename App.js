4c19a14a71d8d30938d791d6a58fcab52ebd8792
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-03-17 17:39:58 +0000

    Trying to get user location to work

--- App.js
+++ App.js
@@ -2,21 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
-// import { Marker } from 'react-native-maps';
 import { locations } from './locations.js'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Location, Permissions } from 'expo';
 
-export default function App() {
+function HomeScreen() {
   return (
     <View style={styles.container}>
       
       <StatusBar style="auto" />
       <MapView style={styles.map}
     initialRegion={{
-      latitude: -33.8915,
-      longitude: 151.2767,
+      latitude: 51.509865,
+      longitude: -0.118092,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     }}
+    showsUserLocation={true}
+
     >
      
       {locations.map((location, index) => {
@@ -39,6 +43,27 @@ export default function App() {
   );
 }
 
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Locality'}}/>
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -48,7 +73,8 @@ const styles = StyleSheet.create({
   },
   container2: {
     flex: 1,
-    backgroundColor: '#fff',
+    width: Dimensions.get('window').width,
+    backgroundColor: '#FA7E61',
     alignItems: 'center',
     justifyContent: 'center'
   },
@@ -58,3 +84,5 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height,
   },
 });
+
+export default App;
