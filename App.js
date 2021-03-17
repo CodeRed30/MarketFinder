f3f21326c5a8142b3eaf6ba90c082038f887c1bd
jshields123 <jackshields@live.co.uk>
2021-03-17 11:32:44 +0000

    added in google map api for ios

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MapView style={styles.map}
+    initialRegion={{
+      latitude: -33.8915,
+      longitude: 151.2767,
+      latitudeDelta: 0.922,
+      longitudeDelta: 0.0421,
+    }}
+  />
     </View>
+    
   );
 }
 
@@ -17,5 +27,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
