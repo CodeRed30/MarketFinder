4698d8b0b2e6ab83b5966d7a836711b3de819fb4
CodeRed30 <katrinaharradine@gmail.com>
2021-03-20 14:17:35 +0000

    New class MarketList

--- app/MarketList.js
+++ app/MarketList.js
@@ -5,86 +5,49 @@ import MarketData from '../src/marketData.js'
 import Constants from 'expo-constants';
 import React, { Component, useState, useEffect } from "react";
 
-let backendUrl = Constants.manifest.extra.backendUrl
-
- let markets = []
-
-// axios({
-//   url: backendUrl + "/markets",
-//   method: 'get'
-// }).then(response => {
-//   for (let i = 0; i < response.data.length; i++) {
-//     let market = new MarketData()
-//     market.addName(response.data[i].name)
-//     market.addDescription(response.data[i].description)
-//     markets.push(market)
-//   }
-// }).catch(function (error) {
-//   console.log(error);
-// });
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  container2: {
-    flex: 1,
-    width: Dimensions.get('window').width,
-    backgroundColor: '#FA7E61',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  map: {
-    flex: 1,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-  listItem: {
-    fontSize: 18,
-  },
-  header: {
-    fontSize: 28,
-    fontWeight: "bold",
-    paddingBottom: 12,
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markets: [],
+      isLoading: false,
+    };
   }
-});
 
-export default MarketList = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-  console.log(data)
-  useEffect(() => {
-    fetch(backendUrl + '/markets')
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
+  fetchMarkets = async () => {
+    this.setState({ isLoading: true });
+    try {
+      const res = await fetch('http://192.168.1.110:3000/markets');
+      const markets = await res.json();
+      this.setState({ markets });
+    } catch (err) {
+      console.log(err);
+    }
+    this.setState({ isLoading: false });
+  };
 
+  componentDidMount(){
+    this.fetchMarkets();
+  };
+  
+  render() {
+    return (
 
-  return (
-
-    <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <Text>Loading...</Text> : 
-      ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
-          <Text style={styles.header}>Nearby Markets:</Text>
-          <FlatList
-            data={data}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-              // key is not being read but is not crashing
-              <Text style={styles.listItem} key={item}>{item.name + ': ' + item.description}</Text>
-            )}
-          />
-        </View>
-      )}
-    </View>
-  );
-};
-
-
-
-// export default MarketList;
+      <View style={{ flex: 1, padding: 24 }}>
+        {this.isLoading ? <Text>Loading...</Text> : 
+        ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+            <Text>Nearby Markets:</Text>
+            <FlatList
+              data={this.state.markets}
+              keyExtractor={({ id }, index) => id}
+              renderItem={({ item }) => (
+                // key is not being read but is not crashing
+                <Text key={item.id}>{item.name}</Text>
+              )}
+            />
+          </View>
+        )}
+      </View>
+    );
+  }
+}
