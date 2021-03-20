c013d7e3ebb8cd1dd569c0d51086c754ec6d28c6
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-03-20 11:15:33 +0000

    Map now shoes route to sample market

--- App.js
+++ App.js
@@ -35,7 +35,8 @@ export default class Map extends React.Component {
   state = {
     latitude: null,
     longitude: null,
-    markets: markets
+    markets: markets,
+    coords: null
   }
 
   async componentDidMount() {
@@ -44,6 +45,7 @@ export default class Map extends React.Component {
     if (status !== 'granted') {
       const response = await Permissions.askAsync(Permissions.LOCATION)
     }
+
     navigator.geolocation.getCurrentPosition(
       ({ coords: { latitude, longitude } }) => this.setState({ latitude, longitude }, this.mergeCoords),
       (error) => console.log('Error:', error)
@@ -56,7 +58,7 @@ export default class Map extends React.Component {
       desLongitude: sampleMarket.lng
     }, this.mergeCoords)
   }
-
+  
   mergeCoords = () => {
     const {
       latitude,
@@ -66,7 +68,6 @@ export default class Map extends React.Component {
     } = this.state
 
     const hasStartAndEnd = latitude !== null && desLatitude !== null
-
     if (hasStartAndEnd) {
       const concatStart = `${latitude},${longitude}`
       const concatEnd = `${desLatitude},${desLongitude}`
@@ -78,10 +79,7 @@ export default class Map extends React.Component {
     try {
       const resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${desLoc}&key=${googleApi}`)
       const respJson = await resp.json();
-      const response = respJson.routes[0]
-      const distanceTime = response.legs[0]
-      const distance = distanceTime.distance.text
-      const time = distanceTime.duration.text
+      console.log(respJson)
       const points = Polyline.decode(respJson.routes[0].overview_polyline.points);
       const coords = points.map(point => {
         return {
@@ -89,26 +87,18 @@ export default class Map extends React.Component {
           longitude: point[1]
         }
       })
-      this.setState({ coords, distance, time })
+      this.setState({ coords })
     } catch(error) {
       console.log('Error: ', error)
     }
-  }
-
-  onMarkerPress = location => () => {
-    const { coords: { latitude, longitude } } = location
-    this.setState({
-      destination: location,
-      desLatitude: latitude,
-      desLongitude: longitude
-    }, this.mergeCoords)
   }
 
   render(){
     const { latitude, longitude, coords } = this.state
     if (latitude) {
       return (
-    <MapView 
+    <MapView
+      provider={MapView.PROVIDER_GOOGLE} 
       showsUserLocation
       style={{ flex: 1 }}
       initialRegion={{
