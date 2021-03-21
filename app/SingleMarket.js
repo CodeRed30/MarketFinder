df698ae3e20e40d066c5dd2236814f8c32e21628
CodeRed30 <katrinaharradine@gmail.com>
2021-03-21 15:39:58 +0000

    Single Market page extra

---
+++ app/SingleMarket.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { Image, View, StyleSheet, Text, ScrollView, Button } from 'react-native';
+// import { Left, Right, Container, H1} from 'native-base'
+
+const SingleMarket = (props) => {
+
+    const [item, setItem] = useState(props.route.params.item)
+
+    return (
+        <Container style={styles.container}>
+            <ScrollView style={{ marginBottom: 80, padding:5 }}>
+                <View>
+                    <Image 
+                        source={{
+                            uri: item.image ? item.image 
+                            : 'https://media-cdn.tripadvisor.com/media/photo-s/17/94/de/ed/caption.jpg'
+                        }}
+                        resizeMode="contain"
+                        style={styles.image}
+                    />
+                    
+                </View>
+
+            </ScrollView>
+ 
+        </Container>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'relative',
+        height: '100%'
+    },
+    imageContainer: {
+        backgroundColor: 'white',
+        padding: 0,
+        margin: 0
+    },
+    image: {
+        width: '100%',
+        height: 250
+    }
+})
+
+export default SingleMarket
