c2d63c623421cc6d8d1dfd4c4961654b81cbaa56
CodeRed30 <katrinaharradine@gmail.com>
2021-03-22 14:43:21 +0000

    Custom Forms

--- app/AddMarket.js
+++ app/AddMarket.js
@@ -1,15 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import { Image, View, StyleSheet, Text, ScrollView, Button } from 'react-native';
-import { Left, Right, Container, H1} from 'native-base'
+import React, { useState, useEffect } from "react"
+import { 
+    View, 
+    Text,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+    Dimensions,
+    Platform,
+} from "react-native"
+import { Item, Picker } from "native-base"
+import FormContainer from './Form/FormContainer';
+import Input from './Form/Input';
+import Icon from "react-native-vector-icons/FontAwesome"
+// import Toast from "react-native-toast-message"
+// import AsyncStorage from "`@react-native-community/async-storage`"
+// import axios from "axios"
+// import * as ImagePicker from "expo-image-picker"
+// import mime from "mime";
 
+var { width } = Dimensions.get('window');
 
-const AddMarket = () => {
+const AddMarket = (props) => {
+
+    const [pickerValue, setPickerValue] = useState();
+    const [name, setName] = useState();
+    const [website, setWebsite] = useState();
+    const [lat, setLat] = useState();
+    const [lng, setLng] = useState();
+    const [weekday_text, setWeekday_text] = useState();
+    const [formatted_address, setFormatted_address] = useState();
+    const [image, setImage] = useState();
+    const [description, setDescription] = useState();
+    const [item, setItem] = useState();
+    //Categories to be added for search
+
 
     return (
-        <Text> Hello Scrummy </Text>
+        <FormContainer title="Add Market">
+            <View>
+                <TouchableOpacity>
+                    <Text>Hello Scrummy!</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.label}>
+               <Text style={{ textDecorationLine: "underline"}}>Name</Text>
+           </View>
+            <View>
+            <Input 
+            placeholder="Market Name"
+            name="name"
+            id="name"
+            value={name}
+            onChangeText={(text) => setName(text)}
+           />
+            </View>
+
+        </FormContainer>
+
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 100,
+        marginBottom: 400,
+        width: width,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
 export default AddMarket;
+
+
+
 // Name 
 // Location
 // Image
