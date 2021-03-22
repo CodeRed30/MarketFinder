8325fddb0358113a1850c71d172e07987b738d02
CodeRed30 <katrinaharradine@gmail.com>
2021-03-22 23:16:02 +0000

    Header for app

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Main from './navigators/Main'
+import Main from './navigators/Main';
+import Header from "./shared/Header";
 
 const Stack = createStackNavigator();  
 
 export default function App() {
   return (
     <NavigationContainer>
+        <Header />
         <Main />
     </NavigationContainer>
   );
