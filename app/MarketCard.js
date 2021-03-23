10b97de1240fd20465cbac1df9dfbbdac654e0c3
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-03-23 13:25:10 +0000

    Markets arranged by shortest walking time first

--- app/MarketCard.js
+++ app/MarketCard.js
@@ -13,48 +13,49 @@ export default class MarketCard extends React.Component {
         name: this.props.name,
         desLatitude: this.props.lat,
         desLongitude: this.props.lng,
+        time: this.props.time
     }
 
-    mergeCoords = () => {
-        const {
-          latitude,
-          longitude,
-          desLatitude,
-          desLongitude,
-        } = this.state
+    // mergeCoords = () => {
+    //     const {
+    //       latitude,
+    //       longitude,
+    //       desLatitude,
+    //       desLongitude,
+    //     } = this.state
         
-        const hasStartAndEnd = latitude !== null && desLatitude !== null
-        if (hasStartAndEnd) {
-          const concatStart = `${latitude},${longitude}`
-          const concatEnd = `${desLatitude},${desLongitude}`
-          this.getTime(concatStart, concatEnd)
-        }
-      }
+    //     const hasStartAndEnd = latitude !== null && desLatitude !== null
+    //     if (hasStartAndEnd) {
+    //       const concatStart = `${latitude},${longitude}`
+    //       const concatEnd = `${desLatitude},${desLongitude}`
+    //       this.getTime(concatStart, concatEnd)
+    //     }
+    //   }
     
-      async getTime(startLoc, desLoc) {
-        try {
-          const resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${desLoc}&mode=walking&key=${googleApi}`)
-          const respJson = await resp.json();
-          const response = respJson.routes[0]
-          const distanceTime = response.legs[0]
-          const time = distanceTime.duration.text
-          this.setState({
-            time: time,
-          });
-        } catch(error) {
-          console.log('Error: ', error)
-        }
-      }
+    //   async getTime(startLoc, desLoc) {
+    //     try {
+    //       const resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${desLoc}&mode=walking&key=${googleApi}`)
+    //       const respJson = await resp.json();
+    //       const response = respJson.routes[0]
+    //       const distanceTime = response.legs[0]
+    //       const time = distanceTime.duration.text
+    //       this.setState({
+    //         time: time,
+    //       });
+    //     } catch(error) {
+    //       console.log('Error: ', error)
+    //     }
+    //   }
 
-      componentDidMount() {
-        navigator.geolocation.getCurrentPosition(
-            ({ coords: { latitude, longitude } }) => this.setState({ latitude, longitude }, this.mergeCoords),
-            (error) => console.log('Error:', error)
-          )
+    //   componentDidMount() {
+    //     navigator.geolocation.getCurrentPosition(
+    //         ({ coords: { latitude, longitude } }) => this.setState({ latitude, longitude }, this.mergeCoords),
+    //         (error) => console.log('Error:', error)
+    //       )
       
           
-        this.mergeCoords;
-      }
+    //     this.mergeCoords;
+    //   }
 
     render() {
         return (
--- app/MarketList.js
+++ app/MarketList.js
@@ -9,6 +9,7 @@ import { Left, Right, Container, H3} from 'native-base';
 
 import MarketCard from './MarketCard'
 
+let googleApi = Constants.manifest.extra.googleApi
 
 export default class App extends Component {
   constructor(props) {
@@ -32,22 +33,73 @@ export default class App extends Component {
       console.log(err);
     }
     this.setState({ isLoading: false });
+
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => this.setState({ latitude, longitude }, this.mergeCoords),
+      (error) => console.log('Error:', error)
+    )
+
   };
 
-  componentDidMount(){
-    this.fetchMarkets();
-  };
+  mergeCoords = async () => {
+    let markets = await Promise.all(this.state.markets.map(async (market) => {
+    const {
+      latitude,
+      longitude
+    } = this.state
+
+    let desLatitude = market.lat
+    let desLongitude = market.lng
+    
+    const hasStartAndEnd = latitude !== null && desLatitude !== null
+    if (hasStartAndEnd) {
+      const concatStart = `${latitude},${longitude}`
+      const concatEnd = `${desLatitude},${desLongitude}`
+      market.time = await this.getTime(concatStart, concatEnd, market)
+    }
+    return market
+  }))
+  markets.sort(function (a, b) {
+    return parseInt(a.time) - parseInt(b.time);
+  });
   
+  this.setState({ markets });
+  }
+
+  async getTime(startLoc, desLoc, market) {
+    try {
+      const resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${desLoc}&mode=walking&key=${googleApi}`)
+      const respJson = await resp.json();
+      const response = respJson.routes[0]
+      const distanceTime = response.legs[0]
+      const time = distanceTime.duration.text
+      return time;
+    } catch(error) {
+      console.log('Error: ', error)
+    }
+  }
+
+  componentDidMount() {
+    this.fetchMarkets();
+    
+    // navigator.geolocation.getCurrentPosition(
+    //     ({ coords: { latitude, longitude } }) => this.setState({ latitude, longitude }, this.mergeCoords),
+    //     (error) => console.log('Error:', error)
+    //   )
+  
+    // this.mergeCoords;
+  }
+
   render() {
     return (
-
       <View style={styles.container}>
         {this.isLoading ? <Text>Loading...</Text> : 
         ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
             <H3 style={{fontWeight: "bold"}}>Nearby Markets:</H3>
               <FlatList
                 data={this.state.markets}
-                keyExtractor={({id}) => this.state.markets.id}
+                keyExtractor={({_id}) => _id}
+                
                 renderItem={({ item }) => (
                   // 
                   <TouchableOpacity
